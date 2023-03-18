532999de093c49944f10772ae7c02e3b96a1958a
Yuliia Hubarenko <xlapushka@ukr.net>
2023-03-18 23:39:40 +0200

    current-icon is working))

--- src/script.js
+++ src/script.js
@@ -1,10 +1,9 @@
 function formatDate(dt, timezone) {
-
   let utcSeconds = parseInt(dt, 10) + parseInt(timezone, 10);
   let utsMilliSeconds = utcSeconds * 1000;
   let date = new Date(utsMilliSeconds).toUTCString();
   return date;
-  
+
   // let hours = date.getHours();
   // let doubleDigitsHours = String(hours).padStart(2, "0");
   // let minutes = date.getMinutes();
@@ -22,6 +21,48 @@ function formatDate(dt, timezone) {
 
   // console.log(`${day} ${doubleDigitsHours}:${doubleDigitsMinutes}`);
   // return `${day} ${doubleDigitsHours}:${doubleDigitsMinutes}`;
+}
+
+function currentIcon(symbol) {
+  switch (symbol) {
+    case "01d":
+      return "fa-solid fa-sun";
+    case "02d":
+      return "fa-solid fa-cloud-sun";
+    case "03d":
+      return "fa-solid fa-cloud";
+    case "04d":
+      return "fa-solid fa-cloud";
+    case "09d":
+      return "fa-cloud-showers-heavy";
+    case "10d":
+      return "fa-solid fa-cloud-sun-rain";
+    case "11d":
+      return "fa-solid fa-cloud-bolt";
+    case "13d":
+      return "fa-solid fa-snowflake";
+    case "50d":
+      return "fa-solid fa-smog";
+
+    case "01n":
+      return "fa-solid fa-moon";
+    case "02n":
+      return "fa-solid fa-cloud-moon";
+    case "03n":
+      return "fa-solid fa-cloud";
+    case "04n":
+      return "fa-solid fa-cloud";
+    case "09n":
+      return "fa-cloud-showers-heavy";
+    case "10n":
+      return "fa-solid fa-cloud-moon-rain";
+    case "11n":
+      return "fa-solid fa-cloud-bolt";
+    case "13n":
+      return "fa-solid fa-snowflake";
+    case "50n":
+      return "fa-solid fa-smog";
+  }
 }
 
 function displayCurrent(response) {
@@ -47,12 +88,19 @@ function displayCurrent(response) {
   windElement.innerHTML = windEl;
 
   let weekDayElement = document.querySelector("#current-weekday-and-time");
-  weekDayElement.innerHTML = formatDate(response.data.dt, response.data.timezone);
+  weekDayElement.innerHTML = formatDate(
+    response.data.dt,
+    response.data.timezone
+  );
+
+  let iconElement = document.querySelector("#current-icon");
+  let curtIcon = currentIcon(response.data.weather[0].icon);
+  iconElement.setAttribute("class", `${curtIcon}`);
 }
 
 let apiKey = "062a09b2ac32c51fd9e8b024e2f69734";
-let latitude = 2.85;
-let longitude = 34.63;
+let latitude = 0.71;
+let longitude = 135.0;
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(displayCurrent);
